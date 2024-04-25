fbbf27b343e6020b7552b253039fcef15559e097
flexagoon <code@fxgn.dev>
2024-04-25 20:49:57 +0300

    refactor: major codebase refactor

--- lib/robot.py
+++ lib/robot.py
@@ -1,26 +1,14 @@
-import pygame
-from math import sqrt
-from lib.ui import Window
+"""Provides a class for storing the state of the robot."""
+
 
 class Robot:
-    def __init__(self, window: Window):
-        self.speed = 0
-        self.max_speed = 60
-        self.turn_speed = 0
-        self.direction = 0
-        self.window = window
+    """Current state of the robot."""
 
-        self.gyro = 0
-        self.us = [200] * 6
-        self.us_max_dist = 200
-        
-    
-    def draw(self):
-        pygame.draw.rect(self.window.screen, (217, 217, 217), (175, 255, 100, 190), border_radius=20)
+    speed: int = 0
+    max_speed: int = 60
+    turn_speed: int = 0
+    direction: int = 0
 
-        self.window.draw_line((200, 255), -45, self.us[0] if self.us[0] < self.us_max_dist else self.us_max_dist)
-        self.window.draw_line((225, 255), 0, self.us[1] if self.us[1] < self.us_max_dist else self.us_max_dist)
-        self.window.draw_line((250, 255), 45, self.us[2] if self.us[2] < self.us_max_dist else self.us_max_dist)
-        self.window.draw_line((175, 350), -90, self.us[3] if self.us[3] < self.us_max_dist else self.us_max_dist)
-        self.window.draw_line((275, 350), 90, self.us[4] if self.us[4] < self.us_max_dist else self.us_max_dist)
-        self.window.draw_line((225, 445), 180, self.us[5] if self.us[5] < self.us_max_dist else self.us_max_dist)
+    gyro: int = 0
+    us: list[int] = [200] * 6
+    us_max_dist: int = 200
--- lib/video.py
+++ lib/video.py
@@ -1,76 +1,110 @@
-import cv2
-import numpy
-import pygame
+"""Provides a class for storing and managing video data."""
+
 import pickle
-import imutils
+from typing import NamedTuple
+
+import cv2
+import imutils  # type: ignore[import-untyped]
+import numpy as np
+import pygame
+from cv2 import UMat
+
+from .ui import Rectangle, Window
+
+
+class VideoCrop(NamedTuple):
+    """A bounding box for video cropping."""
+
+    x: int
+    y: int
+    width: int
+    height: int
 
 
 class Video:
-    def __init__(self, window, crop=None):
-        self.frame = None
-        self.surface = None
-        self.binary = None
+    """Class for storing video data."""
+
+    def __init__(self, window: Window, crop: VideoCrop | None = None) -> None:
+        """Initialize a new video."""
+        self.frame: UMat | None = None
+        self.binary = b""
         self.window = window
         self.new_data = False
         self.crop = crop
 
-    def start(self, i=0):
-        self.obj = cv2.VideoCapture(i)
-        if self.crop is None:
-            self.width = int(self.obj.get(cv2.CAP_PROP_FRAME_WIDTH))
-            self.height = int(self.obj.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        else:
-            self.width = self.crop[2]
-            self.height = self.crop[3]
-
-    def read(self):
-        if self.obj.isOpened():
-            _, self.frame = self.obj.read()
-            if self.crop is not None:
-                self.frame = self.frame[self.crop[1]:self.crop[1]+self.crop[3], self.crop[0]:self.crop[0]+self.crop[2]]
-            if self.frame is not None:
-                self.new_data = True
-        return self.frame
-
-    def draw(self, x, y, width, height):
+    def draw(self, rect: Rectangle) -> None:
+        """Draw the video on the screen."""
         if self.frame is None:
-            pygame.draw.rect(self.window.screen, (0, 0, 0),
-                             (x, y, width, height))
+            pygame.draw.rect(self.window.screen, (0, 0, 0), rect)
         else:
-            self.surface = self.frame
+            surface_data = self.frame.get()
 
-            if width / self.width > height / self.height:
-                self.surface = imutils.resize(self.surface, width=width)
-                new_height = self.height * (width / self.width)
-                crop = (new_height - height) / 2
-                self.surface = self.surface[int(crop): int(
-                    new_height - crop), 0: width]
+            if rect.width / self.width > rect.height / self.height:
+                surface_data = imutils.resize(surface_data, width=rect.width)
+                new_height = self.height * (rect.width / self.width)
+                crop = (new_height - rect.height) / 2
+                surface_data = surface_data[
+                    int(crop) : int(new_height - crop),
+                    0 : rect.width,
+                ]
             else:
-                self.surface = imutils.resize(self.surface, height=height)
-                new_width = self.width * (height / self.height)
-                crop = (new_width - width) / 2
-                self.surface = self.surface[0: height, int(
-                    crop): int(new_width - crop)]
+                surface_data = imutils.resize(surface_data, height=rect.height)
+                new_width = self.width * (rect.height / self.height)
+                crop = (new_width - rect.width) / 2
+                surface_data = surface_data[
+                    0 : rect.height,
+                    int(crop) : int(new_width - crop),
+                ]
 
-            self.surface = cv2.cvtColor(self.surface, cv2.COLOR_BGR2RGB)
-            self.surface = numpy.rot90(self.surface)
-            self.surface = pygame.surfarray.make_surface(self.surface)
-            self.window.screen.blit(self.surface, (x, y))
-            pygame.draw.rect(self.window.screen, (0, 0, 0),
-                             (x, y, width, height), 1)
+            surface_data = cv2.cvtColor(surface_data, cv2.COLOR_BGR2RGB)
+            surface_data = np.rot90(surface_data)
+            surface = pygame.surfarray.make_surface(surface_data)
+            self.window.screen.blit(surface, (rect.x, rect.y))
+            pygame.draw.rect(self.window.screen, (0, 0, 0), rect, 1)
 
-    def get_binary(self, width=200):
-        self.binary = self.frame
-        self.binary = imutils.resize(self.binary, width=width)
-        self.binary = pickle.dumps(self.binary)
-        return self.binary
+    def pack(self, width: int = 200) -> None:
+        """Convert the video frame to binary data."""
+        if self.frame is not None:
+            frame_data = self.frame.get()
+            self.binary = imutils.resize(frame_data, width=width)
+            self.binary = pickle.dumps(self.binary)
 
-    def unpack(self):
+    def unpack(self) -> None:
+        """Get the video frame from binary data."""
         buf = pickle.loads(self.binary)
         self.width = buf.shape[1]
         self.height = buf.shape[0]
         self.frame = buf
-        return self.frame
 
-    def close(self):
+
+class UserVideo(Video):
+    """Video data received from user's camera."""
+
+    def __init__(self, window: Window, crop: VideoCrop | None = None) -> None:
+        """Initialize a new video."""
+        super().__init__(window, crop)
+
+        self.obj = cv2.VideoCapture(0)
+        if self.crop:
+            self.width = self.crop.width
+            self.height = self.crop.height
+        else:
+            self.width = int(self.obj.get(cv2.CAP_PROP_FRAME_WIDTH))
+            self.height = int(self.obj.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    def read(self) -> None:
+        """Read a video frame from the camera."""
+        if self.obj.isOpened():
+            _, frame_data = self.obj.read()
+            if self.crop:
+                frame_data = frame_data[
+                    self.crop.y : self.crop.y + self.crop.height,
+                    self.crop.x : self.crop.x + self.crop.width,
+                ]
+            if frame_data is not None:
+                self.frame = UMat(frame_data)  # type: ignore[call-overload]
+                self.new_data = True
+
+    def close(self) -> None:
+        """Close the opencv video capture."""
         self.obj.release()
